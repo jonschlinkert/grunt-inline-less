cb112ba4a50b86f1d2bfc04fe79cc0d0e009ecc5
Lucas Wiener <lucas@wiener.se>
2013-09-03 10:18:44 +0200

    Commented some code

--- tasks/inline.js
+++ tasks/inline.js
@@ -1,19 +1,8 @@
-/*
- * grunt-contrib-concat
- * http://gruntjs.com/
- *
- * Copyright (c) 2012 "Cowboy" Ben Alman, contributors
- * Licensed under the MIT license.
- */
-
 'use strict';
 
 module.exports = function(grunt) {
-
-  var path = require('path');
-  var chalk = require('chalk');
-  var _ = require('lodash');
-  var Tree = require('./lib/tree.js');
+  var chalk = require('chalk');         //Required to output colored messages.
+  var Tree = require('./lib/tree.js');  //Required to parse and build dependency tree.
 
   grunt.registerMultiTask('inline', 'Inline less files.', function() {
     var options = this.options({
@@ -25,10 +14,21 @@ module.exports = function(grunt) {
     // Iterate over all src-dest file pairs.
     this.files.forEach(function(f) {
 
+      //The idea is to first create temporary content (not written to a file) that will import
+      //all the src files. Then parse the content with a dependency tree which. The key here 
+      //is to parse all src files with a root tree so that duplicate dependencies will be removed
+      //cross-src-files. If a tree would be built for each src file and then concatinated, duplicates
+      //could occur in the dest file.
+      
+      //Create the temporary base content by concatinating import statements that imports each src file.
+      //By concatinating the imports with given separator, in the end this will result in concatinated
+      //less files without duplicate imports, separated by given separator.
       var base = grunt.file.expand(f.src).map(function(src) {
+        //Since css imports are kept, the src files are forced to be imported as less.
         return '@import (less) "' + src + '";';
       }).join(options.separator);
 
+      //Validate the base content.
       if(!base) {
         grunt.log.warn('Failed to read file(s): ' + f.orig.src.join(' '));
         return;
@@ -36,8 +36,7 @@ module.exports = function(grunt) {
       
       grunt.verbose.writeln(chalk.cyan('Building dependency tree for source files:\n' + base));
 
-      //Create a new tree for each src file.
-      // grunt.verbose.writeln(chalk.cyan('Building dependency tree for ' + f.src));
+      //Create a tree for the base content.
       var tree = new Tree(base, grunt);
 
       //Flatten the dependency tree.
@@ -53,6 +52,7 @@ module.exports = function(grunt) {
 
       //Loop all unique dependencies.
       uniqueDependencies.reverse().forEach(function(dependency) {
+        //Only inline dependencies that are less files.
         if(dependency.type === 'less') {
           //Inline the content for the dependency into where the statement was found (the first occurance).
           result = result.replace(dependency.statement, dependency.content);
@@ -60,8 +60,12 @@ module.exports = function(grunt) {
       });
 
       //Loop all dependencies and remove the statements from the content.
+      //The statements that already have been inlined do not exist any more,
+      //so this will only remove the duplicate dependency statements.
       dependencies.forEach(function(dependency) {
+        //Only remove less dependency statements.
         if(dependency.type === 'less') {
+          //Replace the statement with an empty string.
           result = result.replace(dependency.statement, '');
         }
       });
